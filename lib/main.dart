39df8a42708b2bcbc58883dac98676857e87fe9f
praveen <100416844+PV27062001@users.noreply.github.com>
2022-09-29 14:59:38 +0530

    all changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'dart:async';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:vendbot/screens/auth.dart';
 import 'package:vendbot/screens/boarding_screen.dart';
+import 'package:vendbot/screens/landing_screen.dart';
+import 'package:vendbot/screens/login.dart';
 import 'package:vendbot/screens/registerscreen.dart';
+import 'package:vendbot/screens/signup.dart';
+import 'package:vendbot/utils.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,25 +26,61 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+      scaffoldMessengerKey: Utils.messengerKey,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute:Splashscreen.id,
+      initialRoute: Splashscreen.id,
       routes: {
-        Splashscreen.id:(context)=>Splashscreen(),
-        Boardingscreen.id:(context)=>Boardingscreen(),
-
+        Splashscreen.id: (context) => Splashscreen(),
+        Boardingscreen.id: (context) => Boardingscreen(),
+        LandingPage.id: (context) => LandingPage(),
       },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              return LandingPage();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: SnackBar(content: Text('Something went wrong')),
+              );
+            } else {
+              return const AuthPage();
+            }
+          }),
     );
   }
 }
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({Key? key}) : super(key: key);
-  static const String id='Splashscreen';
+  static const String id = 'Splashscreen';
 
   @override
   State<Splashscreen> createState() => _SplashscreenState();
@@ -42,20 +89,19 @@ class Splashscreen extends StatefulWidget {
 class _SplashscreenState extends State<Splashscreen> {
   @override
   void initState() {
-    Timer(Duration(
-      seconds: 3
-    ),()=>Navigator.pushReplacementNamed(context, Boardingscreen.id),);
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacementNamed(context, Boardingscreen.id),
+    );
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge,overlays: []);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: []);
     return Scaffold(
       backgroundColor: Colors.deepOrangeAccent,
-      body: Center(
-        child:Image.asset('images/logo.png')
-      ),
+      body: Center(child: Image.asset('images/logo.png')),
     );
   }
 }
-
